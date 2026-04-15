cdd1326309c5719ddf9ee9fb5158f7124426229f
ferhatkortak <ferhat@magegames.com>
2026-04-15 17:56:41 +0300

    feat: update.

---
+++ source/__settings__.js
@@ -0,0 +1,21 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2465887.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 283714505, 283714862, 283714863, 283725423, 283846465, 283846467, 283846474, 283846517, 283846518, 284822180, 284922644, 284935168, 285000819 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+];
